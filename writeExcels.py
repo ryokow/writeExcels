dd6249e2c3471e8855e74a30b81cd00e6f5f2675
Ryoka Kuwahara <kuwahara.ryoka@jp.panasonic.com>
2023-05-31 08:45:20 +0900

    Modify writeExcels.py
    replace openpyxl into pywin32 to treat xlsm.

--- writeExcels.py
+++ writeExcels.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import json
-import openpyxl
+import win32com.client as win32
 
 def process_excel_files(folder_path):
     print(folder_path)
@@ -21,21 +21,26 @@ def process_excel_files(folder_path):
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
         if os.path.isfile(file_path) and filename.lower().endswith((".xlsx",".xls",".xlsm")):
-            wb = openpyxl.load_workbook(file_path)
+            excel = win32.Dispatch("Excel.Application")
+            excel.Visible = False
+
+            # ブックを開く
+            workbook = excel.Workbooks.Open(file_path)
 
             # 指定したシート名の存在チェック
-            if sheet_name in wb.sheetnames:
-                sheet = wb[sheet_name]
+            if sheet_name in [sheet.Name for sheet in workbook.Sheets]:
+                sheet = workbook.Sheets(sheet_name)
 
                 # セルの指定と書き込む値をループして処理
                 for cell in cell_data:
                     cell_value = cell_data[cell]
-                    sheet[cell].value = cell_value
+                    sheet.Range(cell).Value = cell_value
 
                 # 変更を保存
-                wb.save(file_path)
+                workbook.Save()
 
-            wb.close()
+            workbook.Close()
+            excel.Quit()
 
 # コマンドライン引数からフォルダパスを取得
 if len(sys.argv) < 2:
